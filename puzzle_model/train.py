51b5e2bdb186d4648505193e06629134c3914d74
Barnabás Osváth <osvath.barnabas@sonrisa.hu>
2021-03-17 09:47:37 +0100

    train on CnnPolicy

--- puzzle_model/train.py
+++ puzzle_model/train.py
@@ -5,11 +5,11 @@ from stable_baselines3 import PPO, DQN
 from stable_baselines3.common.monitor import Monitor
 
 env = Monitor(gym.make('puzzle_gym:puzzle-v0',
-                       img_size=(12, 12), puzzle_size=(3, 3), penalty_for_step=-0.15,
-                       reward_for_completiton=1, positive_reward_coefficient=1.0))
+                       img_size=(36, 36), puzzle_size=(3, 3), dist_type="manhattan", penalty_for_step=-0.15,
+                       reward_for_completiton=20, positive_reward_coefficient=1.0))
 
-model = PPO('MlpPolicy', env, verbose=1)
-model.learn(total_timesteps=10000)
+model = PPO('CnnPolicy', env, verbose=1, learning_rate=0.001, seed=42)
+model.learn(total_timesteps=130000)
 
 obs = env.reset()
 done = False
@@ -22,4 +22,4 @@ while not done:
     rews.append(rewards)
     print(rewards)
     env.render()
-print(rews, i)
+print(rews, i, sum(rews))

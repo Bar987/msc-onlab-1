fb23434a719462df675185a52f1665e9bad9a9b8
Barnabás Osváth <osvath.barnabas@sonrisa.hu>
2021-03-17 09:41:54 +0100

    Print reward and end game when done

--- poc.py
+++ poc.py
@@ -2,7 +2,8 @@ from puzzle_gym.envs.puzzle_env import PuzzleEnv
 
 env = PuzzleEnv(img_size=(9, 9), puzzle_size=(3, 3))
 env.render()
-while True:
+done = False
+while not done:
     n = int(input("Enter n: "))
-    env.step(n)
-    env.render()
+    _, rew, done, ___ = env.step(n)
+    print(rew)
